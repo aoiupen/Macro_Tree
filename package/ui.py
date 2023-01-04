668176ed32a8ab5a0b3f08c1a3ea61c207e16a34
aoiupen <aoiudesign@gmail.com>
2023-01-04 18:10:23 +0900

    small change

--- package/ui.py
+++ package/ui.py
@@ -19,7 +19,7 @@ class UI():
     def setup_ctr(self,MainWindow,app):
         self.app = app
         self.win = MainWindow
-        self.win.setWindowTitle("window title")
+        self.win.setWindowTitle("Macro")
         self.win.setGeometry(100,100,500,400)
         self.ctr_wid = QWidget()
         self.ctr_lay = QHBoxLayout(self.ctr_wid)
@@ -30,7 +30,7 @@ class UI():
         self.act_list = []
         self.act_info_list = [['Save','Ctrl+S','Save application',tw.save],
                          ['Load','Ctrl+O','Load application',tw.load],
-                         ['Execute','','Execute application',tw.exec_inst],
+                         #['Execute','','Execute application',tw.exec_inst],
                          ['Exit','Ctrl+Q','Exit application',tw.set_cls_win]]
         for act_info in self.act_info_list:
             act = QAction(act_info[ShortC.name.value],self.win)
