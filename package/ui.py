f0f07f099c27dc527fc68c99d14a26920af9a59e
aoiupen <aoiudesign@gmail.com>
2023-01-05 10:20:28 +0900

    delete unused file

--- package/ui.py
+++ package/ui.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from package import tree as tr
-from package import func as fn
 import itertools
 from enum import Enum
         
